9feb428b59e288d03c77b449b6422c0b17c12d90
Randy Glasgow <Glasgowrandy@gmail.com>
2024-07-26 13:27:14 -0600

    refactor: Update NewsCard to fetch 20 ticker news instead of 3
    This commit updates the NewsCard component to fetch 20 ticker news instead of 3. This change ensures that more news articles are displayed, providing a richer user experience.

--- components/custom/NewsCard/NewsCard.tsx
+++ components/custom/NewsCard/NewsCard.tsx
@@ -5,7 +5,7 @@ import { getTickerNews } from "@/lib/api/getTickerNews";
 import { NewsCardClient } from "./components/NewsCard.client";
 
 export const NewsCard: FC<{ symbol: string }> = async ({ symbol }) => {
-  const data = await getTickerNews(symbol, 3);
+  const data = await getTickerNews(symbol, 20);
 
   return <NewsCardClient initData={data} symbol={symbol} />;
 };
