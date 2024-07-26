85c27617310a5728903f1c565ae25463863cf606
Randy Glasgow <Glasgowrandy@gmail.com>
2024-07-26 11:18:00 -0600

    refactor: Simplify getSymbolInfo API call in getSymbolInfo.tsx
    This commit simplifies the getSymbolInfo API call in the getSymbolInfo.tsx file. It replaces the previous URL construction with a more concise and readable format. This change improves the code's maintainability and reduces the potential for errors.

--- api/SymbolInfo/getSymbolInfo.tsx
+++ api/SymbolInfo/getSymbolInfo.tsx
@@ -2,9 +2,7 @@ import { API_URL } from '@/constants/endpoints';
 import { API } from '@/lib/types/types';
 
 export async function getSymbolInfo(symbol: string, date?: string) {
-  const res = await fetch(
-    `${API_URL}/symbol_info/${date}?symbol=${symbol}`
-  );
+  const res = await fetch(`${API_URL}/gateway/${symbol}`);
 
   if (!res.ok) throw new Error("Error getting symbol info");
 
--- app/research/[symbol]/page.tsx
+++ app/research/[symbol]/page.tsx
@@ -1,8 +1,22 @@
+import { get } from "http";
+
+import { getSymbolInfo } from "@/api/SymbolInfo/getSymbolInfo";
 import { PageLayout } from "@/components/core/layouts/page";
 import { GlobalNavBar } from "@/components/custom/GlobalNavBar/GlobalNavBar";
 import { NewsCard } from "@/components/custom/NewsCard/NewsCard";
 import { TimeSeriesChart } from "@/components/custom/TimeSeriesChart/TimeSeriesChart";
+import { d } from "@tanstack/react-query-devtools/build/legacy/devtools-PtxSnd7z";
 
+export async function generateMetadata({ params }) {
+  const data = await getSymbolInfo(params.symbol.toUpperCase());
+  return {
+    title: `${params.symbol.toUpperCase()}: ${data.results?.name}`,
+    description: data.results?.description,
+    image: data.results?.branding?.logo_url,
+    // set the favicon
+    favicon: data.results?.branding?.icon_url,
+  };
+}
 const SymbolResearchPage = ({
   params,
 }: {
